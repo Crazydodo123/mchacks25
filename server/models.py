373dc16b9a5e4853513549239f3aee7c12514c43
Leah H <leah@Leahs-MacBook-Pro.local>
2025-01-25 23:42:39 -0500

    mysql data base set up, backend update

---
+++ server/models.py
@@ -0,0 +1,20 @@
+from . import db
+from datetime import datetime as dt
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    total_amount = db.Column(db.Float, default = 0.0)
+
+class Transactions(db.Model):
+    id = id.Column(db.Intger, primary_key = True)
+    user_owed_id = db.Column(db.String(100), foreign_key = ('user.username'), nullable=False)
+    user_owing_id = db.Column(db.String(100), foreign_key = ('user.username'), nullable = False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+    transaction_dt = db.Column(db.DateTime, default = dt.now)
+
+    user_owed = db.relationship('User', foreign_keys=[user_owed_id])
+    user_owing = db.relationship('User', foreign_keys=[user_owing_id])

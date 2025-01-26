63184d0aa8e13086206ad8d318c4d8177d940c80
Dominic Lemoine de Martigny <dodolem0306@gmail.com>
2025-01-26 10:39:13 -0500

    email fix

--- server/debt.py
+++ server/debt.py
@@ -52,8 +52,8 @@ def add_debt():
     user_owing.total_amount -= amount
     if user_owing.total_amount <= -50:
         url = "https://api.gumloop.com/api/v1/start_pipeline"
-        email = data.get('email')
-        message = "The user's name is " + data.get('user_id') + " and they are " + str(user_owing.total_amount) + " dollars in debt with no signs of paying it back to their friends anytime soon."
+        email = user_owing.email
+        message = "The user's name is " + email + " and they are " + str(user_owing.total_amount) + " dollars in debt with no signs of paying it back to their friends anytime soon."
         payload = {
             "user_id": "xAOao0fOp4chdJeXLOMy28LBiim1",
             "saved_item_id": "5kRukEgUKGUyADokZTcL2v",

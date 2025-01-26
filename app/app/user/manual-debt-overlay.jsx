06d2b1989188e2ea4c659530e936ccffaed4d973
Kiersten E <152218847+kiengen@users.noreply.github.com>
2025-01-26 11:30:51 -0500

    manual entry mvp

--- app/app/user/manual-debt-overlay.jsx
+++ app/app/user/manual-debt-overlay.jsx
@@ -2,10 +2,10 @@ import { View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native'
 import { router, useLocalSearchParams } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 import { useState, useContext} from "react";
-import SettingsList from "../context/setting-list";
+import SettingsList from "../context/setting-list.js";
 
 export default function DebtOverlay() {
-    const { email } = useLocalSearchParams<{ email: string }>();
+    const { email } = useLocalSearchParams();
     const settings = useContext(SettingsList);
     const s = require("../../styles.js");
     const [ page, setPage ] = useState(0);
@@ -17,6 +17,20 @@ export default function DebtOverlay() {
     const [ tempItemPrice, setTempItemPrice ] = useState("");
     const [ useLump, setUseLump ] = useState(true);
     const [ costs, setCosts ] = useState([]);
+
+    const [ split, setSplit ] = useState({});
+
+    // split values taken as strings
+    const logNewDebt = () => {
+        // verify that it adds up to the sum
+        console.log(split)
+        router.back()
+        // add debt
+    }
+
+    const splitEvenly = () => {
+        // do the math and reassign split
+    }
     
     return (
         page == 0 ?
@@ -41,7 +55,7 @@ export default function DebtOverlay() {
             
             <TextInput style={s.textInput} placeholder="name" placeholderTextColor={"lightgray"} value={tempName} onChangeText={(newText) => {setTempName(newText)}}></TextInput>
             <TouchableOpacity style={s.hozButton} onPress={() => {setNames([...names, tempName]); setTempName("")}}><Text style={s.buttonText}>Add Person</Text></TouchableOpacity>
-            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page+1)}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
+            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page+1); let tempSplit = {}; console.log(names); for(let i = 0; i < names.length; i++) {tempSplit[names[i]] = 0}; setSplit(tempSplit)}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
         </View> :
         page == 1 ?
         <View style={s.mainScreenB}>
@@ -81,49 +95,34 @@ export default function DebtOverlay() {
                 <TextInput style={s.textInput} placeholder="price" placeholderTextColor={"lightgray"} inputMode={"decimal"} value={tempItemPrice} onChangeText={(newText) => {setTempItemPrice(newText)}}></TextInput>
                 <TouchableOpacity style={s.hozButton} onPress={() => {setNames([...names, tempName]); setTempName("")}}><Text style={s.buttonText}>Add Item</Text></TouchableOpacity>
             </View>}
-            
-            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page+1)}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
+            <View style={{backgroundColor: "rgb(118, 199, 138)", height: 1, width: "60%", margin: 10}}></View>
+            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page+1); console.log(split)}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
+            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page-1)}}><Text style={s.buttonText}>Back</Text></TouchableOpacity>
         </View> :
         page == 2 ?
         <View style={s.mainScreenB}>
             <Text style={s.styleTextEmph}>How is it split?</Text>
-            <Text style={{...s.plainText, color: "rgb(118, 199, 138)", paddingTop: 10}}>Did everyone pay evenly?</Text>
+            <Text style={{...s.plainText, color: "rgb(118, 199, 138)", paddingTop: 10}}>Did everyone share evenly?</Text>
             <Text style={{...s.plainText, color: "rgb(118, 199, 138)"}}>Put the split below.</Text>
             <View style={s.hbox}>
-                <TouchableOpacity onPress={() => {setUseLump(true)}} style={{flexDirection: "row", alignItems: "center"}}>
-                    <Feather style={{...s.plainText, color: "rgb(118, 199, 138)"}} name={useLump ? "check-circle" : "circle"}/>
-                    <Text style={{...s.plainText, color: "rgb(118, 199, 138)", padding: 8}}>Lump Sum</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {setUseLump(false)}} style={{flexDirection: "row", alignItems: "center"}}>
-                    <Feather style={{...s.plainText, color: "rgb(118, 199, 138)"}} name={useLump ? "circle" : "check-circle"}/>
-                    <Text style={{...s.plainText, color: "rgb(118, 199, 138)", padding: 8}}>Item List</Text>
-                </TouchableOpacity>
+                <TouchableOpacity style={s.hozButton} onPress={() => {splitEvenly()}}><Text style={s.buttonText}>Split Evenly</Text></TouchableOpacity>
             </View>
             <View style={{backgroundColor: "rgb(118, 199, 138)", height: 1, width: "60%", margin: 10}}></View>
-            <Text style={s.styleTextEmph}>{useLump ? "Lump Sum" : "Items Purchased"}</Text>
-            {useLump ?
-            <View>
-                <TextInput style={s.textInput} inputMode={"decimal"} placeholder="sum" placeholderTextColor={"lightGray"}></TextInput>
-            </View> :
-            <View style={{width: "100%", alignItems: "center"}}>
-                <View style={{width: "70%", height: 120, backgroundColor: "white", margin: 10, borderWidth: 1, borderColor: "rgb(118, 199, 138)"}}>
-                    <FlatList
-                        contentContainerStyle={{padding: 10}}
-                        data={costs}
-                        renderItem={({item, index}) =>
-                            <View style={{...s.hbox, justifyContent: "space-between", alignItems: "center"}}>
-                                <Text>{item}</Text>
-                                <TouchableOpacity onPress={() => {var newNames = names; newNames.splice(index, 1); setNames(newNames);}}><Feather name="x"/></TouchableOpacity>
-                            </View>
-                        }
-                    />
-                </View>
-                <TextInput style={s.textInput} placeholder="name" placeholderTextColor={"lightgray"} value={tempItemName} onChangeText={(newText) => {setTempItemName(newText)}}></TextInput>
-                <TextInput style={s.textInput} placeholder="price" placeholderTextColor={"lightgray"} inputMode={"decimal"} value={tempItemPrice} onChangeText={(newText) => {setTempItemPrice(newText)}}></TextInput>
-                <TouchableOpacity style={s.hozButton} onPress={() => {setNames([...names, tempName]); setTempName("")}}><Text style={s.buttonText}>Add Item</Text></TouchableOpacity>
-            </View>}
-            
-            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page+1)}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
+            <Text style={s.styleTextEmph}>Cost Split</Text>
+            <View style={{width: "70%", height: 240, margin: 10, borderBottomWidth: 1, borderTopWidth: 1, borderColor: "rgb(118, 199, 138)"}}>
+                <FlatList
+                    contentContainerStyle={{padding: 10}}
+                    data={Object.keys(split)}
+                    renderItem={({item, index}) =>
+                        <View style={{...s.hbox, justifyContent: "space-between", alignItems: "center"}}>
+                            <Text>{item}</Text>
+                            <TextInput style={s.textInput} inputMode={"decimal"} value={split[item]} onChangeText={(newText) => {setSplit({...split, [item]: newText})}}></TextInput>
+                        </View>
+                    }
+                />
+            </View>
+            <TouchableOpacity style={s.hozButton} onPress={() => {logNewDebt()}}><Text style={s.buttonText}>Next</Text></TouchableOpacity>
+            <TouchableOpacity style={s.hozButton} onPress={() => {setPage(page-1)}}><Text style={s.buttonText}>Back</Text></TouchableOpacity>
         </View> :
         <View/> 
     );
